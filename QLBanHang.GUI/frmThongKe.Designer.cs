43836f55aecca416bf82ea00947c88f866efb992
Thanh Dang <dangn.thanh2005@gmail.com>
2026-01-07 14:37:26 +0700

    - git small change on UI

--- QLBanHang.GUI/frmThongKe.Designer.cs
+++ QLBanHang.GUI/frmThongKe.Designer.cs
@@ -58,7 +58,7 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Location = new System.Drawing.Point(12, 8);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(710, 362);
@@ -73,9 +73,9 @@
             this.tabPage1.Controls.Add(this.dtpDenNgay);
             this.tabPage1.Controls.Add(this.dtpTuNgay);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
             this.tabPage1.Size = new System.Drawing.Size(702, 336);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Hóa Đơn Theo Ngày";
@@ -105,7 +105,7 @@
             // 
             this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvHoaDon.Location = new System.Drawing.Point(4, 101);
-            this.dgvHoaDon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvHoaDon.Margin = new System.Windows.Forms.Padding(2);
             this.dgvHoaDon.Name = "dgvHoaDon";
             this.dgvHoaDon.RowHeadersWidth = 62;
             this.dgvHoaDon.RowTemplate.Height = 28;
@@ -115,7 +115,7 @@
             // btnThongKeHD
             // 
             this.btnThongKeHD.Location = new System.Drawing.Point(360, 44);
-            this.btnThongKeHD.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThongKeHD.Margin = new System.Windows.Forms.Padding(2);
             this.btnThongKeHD.Name = "btnThongKeHD";
             this.btnThongKeHD.Size = new System.Drawing.Size(109, 24);
             this.btnThongKeHD.TabIndex = 2;
@@ -126,7 +126,7 @@
             // dtpDenNgay
             // 
             this.dtpDenNgay.Location = new System.Drawing.Point(360, 13);
-            this.dtpDenNgay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpDenNgay.Margin = new System.Windows.Forms.Padding(2);
             this.dtpDenNgay.Name = "dtpDenNgay";
             this.dtpDenNgay.Size = new System.Drawing.Size(193, 20);
             this.dtpDenNgay.TabIndex = 1;
@@ -134,7 +134,7 @@
             // dtpTuNgay
             // 
             this.dtpTuNgay.Location = new System.Drawing.Point(102, 13);
-            this.dtpTuNgay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpTuNgay.Margin = new System.Windows.Forms.Padding(2);
             this.dtpTuNgay.Name = "dtpTuNgay";
             this.dtpTuNgay.Size = new System.Drawing.Size(195, 20);
             this.dtpTuNgay.TabIndex = 0;
@@ -143,9 +143,9 @@
             // 
             this.tabPage2.Controls.Add(this.dgvTopSanPham);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
             this.tabPage2.Size = new System.Drawing.Size(702, 336);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Top 3 Bán Chạy";
@@ -153,9 +153,12 @@
             // 
             // dgvTopSanPham
             // 
+            this.dgvTopSanPham.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvTopSanPham.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvTopSanPham.Location = new System.Drawing.Point(2, 2);
-            this.dgvTopSanPham.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvTopSanPham.Margin = new System.Windows.Forms.Padding(2);
             this.dgvTopSanPham.Name = "dgvTopSanPham";
             this.dgvTopSanPham.RowHeadersWidth = 62;
             this.dgvTopSanPham.RowTemplate.Height = 28;
@@ -166,9 +169,9 @@
             // 
             this.tabPage3.Controls.Add(this.dgvDoanhThu);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
-            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(2);
             this.tabPage3.Size = new System.Drawing.Size(702, 336);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Doanh Thu Tháng";
@@ -176,13 +179,16 @@
             // 
             // dgvDoanhThu
             // 
+            this.dgvDoanhThu.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvDoanhThu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDoanhThu.Location = new System.Drawing.Point(4, 4);
-            this.dgvDoanhThu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvDoanhThu.Margin = new System.Windows.Forms.Padding(2);
             this.dgvDoanhThu.Name = "dgvDoanhThu";
             this.dgvDoanhThu.RowHeadersWidth = 62;
             this.dgvDoanhThu.RowTemplate.Height = 28;
-            this.dgvDoanhThu.Size = new System.Drawing.Size(504, 250);
+            this.dgvDoanhThu.Size = new System.Drawing.Size(694, 328);
             this.dgvDoanhThu.TabIndex = 0;
             // 
             // frmThongKe
@@ -191,7 +197,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(733, 381);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmThongKe";
             this.Text = "Thống kê";
             this.tabControl1.ResumeLayout(false);
